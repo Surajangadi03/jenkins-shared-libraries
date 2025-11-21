003decf628d71b9c1a47e502986f58feb1b817f5
Suraj V Angadi <surajvasant03@gmail.com>
2025-11-21 14:06:36 +0530

    Update docker_deploy.groovy

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -1,3 +1,4 @@
 def call(String ProjectName, String ImageTag){
-  sh "docker rm notes-container && docker run -d --name notes-container -p 7000:8000 ${ProjectName}:${ImageTag}"
+  sh "docker rm -f notes-container || true"
+  sh "docker run -d --name notes-container -p 7000:8000 ${ProjectName}:${ImageTag}"
 }
