d2e00ec28fb1ded94c0a3ff31ad309f0c742073b
Suraj V Angadi <surajvasant03@gmail.com>
2025-11-21 12:37:20 +0530

    Update docker_deploy.groovy

--- vars/docker_deploy.groovy
+++ vars/docker_deploy.groovy
@@ -1,3 +1,3 @@
-def call(String ProjectNmae, String ImageTag){
-  sh "docker run -d -p 6000:8000 ${ProjectName}/${ImageTag}"
+def call(String ProjectName, String ImageTag){
+  sh "docker run -d -p 6000:8000 ${ProjectName}:${ImageTag}"
 }
